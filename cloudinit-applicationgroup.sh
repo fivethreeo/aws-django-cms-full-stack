c048c34402e9ebc074063d54bdced18b3abd72d3
Øyvind Saltvik <oyvind.saltvik@gmail.com>
2016-02-10 09:32:55 +0100

    delete file

--- cloudinit-applicationgroup.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# Add WSGIApplicationGroup so lxml does not time us out
-
-if ! grep -q 'WSGIApplicationGroup %%{GLOBAL}' /opt/elasticbeanstalk/hooks/config.py ; then
-  sed -i -e 's/\(WSGIProcessGroup wsgi\)/\1\nWSGIApplicationGroup %%{GLOBAL}/' /opt/elasticbeanstalk/hooks/config.py 
-fi
-
-cat /opt/elasticbeanstalk/hooks/config.py
