7b53af398dd13a71f41627423674d75370811c7b
Øyvind Saltvik <oyvind.saltvik@gmail.com>
2016-02-11 21:16:06 +0100

    no shim

--- assets/main.js
+++ assets/main.js
@@ -1,34 +1,4 @@
-/**
- * Main app initialization and initial auth check
- */
-// Require.js allows us to configure shortcut alias
-require.config({
-	// The shim config allows us to configure dependencies for
-	// scripts that do not call define() to register a module
-	'shim': {
-		'underscore': {
-			'exports': '_'
-		},
-		'backbone': {
-			'deps': [
-				'underscore',
-				'jquery'
-			],
-			'exports': 'Backbone'
-		}
-	},
-	'paths': {
-		'jquery': 'bower_components/jquery/dist/jquery',
-		'underscore': 'bower_components/underscore/underscore',
-		'backbone': 'bower_components/backbone/backbone',
-		'bootstrap': 'bower_components/bootstrap/dist/js/bootstrap',
-		'text': 'bower_components/text/text',
-		'datetimepicker': 'bower_components/bootstrap-datetimepicker/src/js/bootstrap-datetimepicker',
-		'moment': 'bower_components/moment/moment',
-		'moment-nb': 'bower_components/moment/locale/nb'
 
-	}
-});
 require([
     "jquery", "underscore", "bootstrap", "moment", "moment-nb", "datetimepicker"
 ],
